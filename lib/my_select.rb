d87f869d35cfedab3ce5979addc44358f85ef903
Emily Wagner <emilywagner598@gmail.com>
2019-01-19 21:40:33 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,11 @@
 def my_select(collection)
- # your code here!
- i = 0
- result = []
+  # your code here!
+  i = 0
+  result = []
 
- while i < collection.length
-   result << if yield(collect[i]) == true
-   i += 1
- end
- result
+  while i < collection.length
+    result << if yield(collect[i]) == true
+      i += 1
+  end
+  result
 end
